b88f80a04e590ecea6690f4f93adf33d716b1a90
howdiz <howard.ross@gmail.com>
2020-12-14 10:38:41 -0500

    add withServiceWorker
    also put gh token in env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const { withXDN, withServiceWorker } = require('@xdn/next/config')
 
-module.exports = withXDN({
+
+module.exports = withXDN(withServiceWorker(
+  {
   env: {
-    GITHUB_TOKEN: '',
-  },
-})
+    GITHUB_TOKEN: process.env.GH_TOKEN
+    },
+}))
